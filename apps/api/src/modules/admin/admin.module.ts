89b01152bc6cb29cbbef9330ab5a17e978157782
unknown <theapexmeet@gmail.com>
2026-01-20 15:29:25 +0100

    Add ConfigModule import to AdminModule for ConfigService dependency

--- apps/api/src/modules/admin/admin.module.ts
+++ apps/api/src/modules/admin/admin.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { MonnifyService } from '../payments/monnify.service';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [AdminController],
   providers: [AdminService, MonnifyService],
   exports: [AdminService],
